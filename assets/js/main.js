5e7f6b51bb012a5775f05d1df1b4a44d42fc21cb
Riazul Islam <ri64627@gmail.com>
2020-02-29 20:23:45 +0600

    dashboard area deposit design added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const loginButton = document.getElementById("loginbtn");
 
-loginButton.addEventListener("click",function(){
+loginButton.addEventListener("click", function () {
   const loginArea = document.getElementById("login");
   loginArea.style.display = "none";
+  const transationArea = document.getElementById("transation");
+  transationArea.style.display = "block";
 });
