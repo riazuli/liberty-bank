f1d8dee0abf77e4cae4084db1db63bf7b8c0ddf5
Riazul Islam <ri64627@gmail.com>
2020-02-29 22:46:07 +0600

    deposit function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,23 @@
+// Login Event
 const loginButton = document.getElementById("loginbtn");
-
 loginButton.addEventListener("click", function () {
   const loginArea = document.getElementById("login");
   loginArea.style.display = "none";
   const transationArea = document.getElementById("transation");
   transationArea.style.display = "block";
 
-  const depositButton = document.getElementById("depositButton");
-  depositButton.addEventListener("click", function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositAmountNumber = parseFloat(depositAmount);
-    console.log(depositAmountNumber);
-  });
+
+});
+
+// Deposit Event
+const depositButton = document.getElementById("depositButton");
+depositButton.addEventListener("click", function () {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const depositAmountNumber = parseFloat(depositAmount);
+
+  const depositText = document.getElementById("depositText").innerText;
+  const depositTextNumber = parseFloat(depositText); 
+  const totalDepositAmount = depositAmountNumber + depositTextNumber;
+  document.getElementById("depositText").innerText = totalDepositAmount;
+  document.getElementById("depositAmount").value = "";
 });
